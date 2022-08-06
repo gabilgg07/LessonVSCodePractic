ca99a2ff65cd7d212a24f368b5af8108d72768b8
gabilgg07 <gabilgg07@gmail.com>
2022-08-06 20:03:38 +0400

    Sass Practice Page some partial ended

--- 02_Sass/05_Video_5 Sass practice on page/assets/js/index.js
+++ 02_Sass/05_Video_5 Sass practice on page/assets/js/index.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
     e.preventDefault();
     $(".nav-list").slideToggle();
   });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 60) {
+      $("header").css("position", "fixed");
+      $("header").css("background-color", "rgb(255 255 255 / 72%)");
+    } else if ($(window).scrollTop() < 150) {
+      $("header").css("position", "absolute");
+      $("header").css("background-color", "transparent");
+    }
+  });
 });
