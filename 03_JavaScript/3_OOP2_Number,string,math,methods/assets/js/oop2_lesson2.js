71094260cb317960ac2200703244548ebc931e8b
gabilgg07 <gabilgg07@gmail.com>
2022-08-21 18:47:51 +0400

    OOP 2 ended

--- 03_JavaScript/3_OOP2_Number,string,math,methods/assets/js/oop2_lesson2.js
+++ 03_JavaScript/3_OOP2_Number,string,math,methods/assets/js/oop2_lesson2.js
@@ -10,12 +10,14 @@ let arr = [];
 
 let names = ["Qabil", "Vusal", "Selim", "Sevda", "Mursel"];
 
-console.log(`let names = ["Qabil", "Vusal", "Selim", "Sevda", "Mursel"];`);
+console.log("let names = ");
+console.log(names);
 console.log(" ");
 
 // Array-in melumatlarinin sayini(siyahinin uzunlugunu) .length property-si qaytarir:
 
-console.log(`names.length => ${names.length}`);
+console.log("names.length => ");
+console.log(names.length);
 console.log(" ");
 
 // Array-de melumatlar index-lenmis sekilde saxlanilir.
@@ -31,19 +33,19 @@ console.log(`names[9] => ${names[9]}`);
 console.log(" ");
 
 // array ucun sade for dongusu:
-console.log("Sade for loop:");
+console.log("Sade For Loop:");
 for (let i = 0; i < names.length; i++) {
   console.log(names[i]);
 }
 console.log(" ");
 
-console.log("for of loop:");
+console.log("For Of Loop:");
 for (const name of names) {
   console.log(name);
 }
 console.log(" ");
 
-console.log("for in loop:");
+console.log("For In Loop:");
 for (const index in names) {
   console.log(`index: ${index}`);
   console.log(`item (names[${index}]): ${names[index]}`);
@@ -53,26 +55,24 @@ console.log(" ");
 // CRUD - mentiqinin tedbiqi:
 
 // C-reate:
-
+console.log(names);
+console.log("Create: ");
 names[5] = "Elman";
 console.log(`names[5] = "Elman";`);
 console.log(names);
-console.log(" ");
 
 // R-ead:
-
+console.log("Read: ");
 console.log(`names[3] => ${names[3]}`);
-console.log(" ");
 
 // U-pdate:
-
+console.log("Update: ");
 names[2] = "Feride";
 console.log(`names[2] = "Feride";`);
 console.log(names);
-console.log(" ");
 
 // D-elete:
-
+console.log("Delete: ");
 delete names[5];
 console.log(`delete names[5];`);
 console.log(names);
@@ -84,7 +84,7 @@ console.log(" ");
 
 // .push(item) => item-i(melumati) sona elave etme:
 let newLength = names.push("Hesen");
-console.log(`names.push("Hesen");`);
+console.log(`let newLength = names.push("Hesen");`);
 console.log(`newLength:${newLength}`);
 console.log(names);
 console.log(" ");
@@ -106,7 +106,7 @@ console.log(" ");
 
 // .unshift(item) => item-i(melumati) evvele elave etme:
 newLength = names.unshift("Hesen");
-console.log(`names.unshift("Hesen");`);
+console.log(`newLength = names.unshift("Hesen");`);
 console.log(`newLength:${newLength}`);
 console.log(names);
 console.log(" ");
@@ -232,11 +232,8 @@ let users = [
   { id: 2, name: "Peter" },
   { id: 3, name: "Mery" },
 ];
-console.log(`let users = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Peter" },
-  { id: 3, name: "Mery" },
-];`);
+console.log("let users = ");
+console.log(users);
 console.log(" ");
 
 // find & findIndex:
@@ -274,15 +271,8 @@ let products = [
   { id: 7, name: "honor P78", category: "phone" },
 ];
 
-console.log(`let products = [
-  { id: 1, name: "samsung galaxy note 10", category: "phone" },
-  { id: 3, name: "samsung 10 tv", category: "television" },
-  { id: 2, name: "iphone 10", category: "phone" },
-  { id: 4, name: "ipad 3", category: "tablet" },
-  { id: 5, name: "acer t543", category: "computer" },
-  { id: 6, name: "lg C321", category: "television" },
-  { id: 7, name: "honor P78", category: "phone" },
-];`);
+console.log("let products = ");
+console.log(products);
 console.log(" ");
 
 // .filter(callback func) => serti odeyen butun itemleri array seklinde qaytarir:
@@ -316,16 +306,105 @@ console.log(sorted_names);
 console.log(" ");
 // numberlerde artan sira ile yigmasi ucun, xususi callback func gondermek lazim:
 
-let unsorted_nums = [5, 23, 1, 76, 35, 234, 35, 675, 21, 8];
-console.log(`let unsorted_nums = [5,23,1,76,35,234,35,675,21,8];`);
-let sorted_nums = unsorted_nums.sort((a, b) => {
+let numsForSorted = [5, 23, 1, 76, 35, 234, 35, 675, 21, 8];
+console.log(`let numsForSorted = `);
+console.log(numsForSorted);
+numsForSorted.sort((a, b) => {
   return a - b;
 });
-console.log(`let sorted_nums = unsorted_nums.sort((a, b) => {
+console.log(`numsForSorted.sort((a, b) => {
   return a - b;
 });`);
 
-console.log(`sorted_nums: `);
-console.log(sorted_nums);
+console.log(`numsForSorted: `);
+console.log(numsForSorted);
+
+console.log(" ");
+
+// .reverse(array) => array-in itemlerini guzgu effekti seklinde, tersine siraliyir:
+let siraSaylari = ["birinci", "ikinci", "ucuncu", "dorduncu"];
+console.log("siraSaylari: ");
+console.log(siraSaylari);
+
+siraSaylari.reverse();
+console.log(`siraSaylari.reverse();`);
+
+console.log("siraSaylari: ");
+console.log(siraSaylari);
+
+console.log(" ");
+
+// {string}.split(delim(etr)) => stringlere tedbiq olunur, delimetrde(splitterde, parametr olaraq) verilmis stringe esasen her tapdigi bu hisseye gore ayiraraq array-e yigir:
+let forSplitStr = "mekteb, sinif, otaq, lovhe, silgi, xerite, kitab, defter";
+console.log(`let forSplitStr = "${forSplitStr}"`);
+let splitedArr = forSplitStr.split(", ");
+console.log(`let splitedArr = forSplitStr.split(", ");`);
+console.log("splitedArr: ");
+console.log(splitedArr);
+console.log(" ");
+
+// .join(glue("; ")) => split-in eksi olaraq, verilmis glue-ye esasen, array-in itemlerini string seklinde birlesdirir:
+let joinedStr = splitedArr.join("; ");
+console.log(`let joinedStr = splitedArr.join("; ");`);
+console.log("joinedStr: ");
+console.log(joinedStr);
+console.log(" ");
+
+// -------- reduce & reduceRight
+
+// Diger forEach-e benzer dongulerdeki kimi, bunlar da callback func qebul edir.
+// item, index, array-den elave ilk olaraq accumulator parametri qebul edir.
+// callback func-dan da elave optional(olmasa da olar) seklinde initial parametri qebul edir.
+
+// .reduce(function(accumulator, item, index, array){...}, [initial]); =>
+// Misal array-imizin icinde ededler var,\ onlari cemleyib 1 eded cem qaytarmaliyiq.
+// initial -> ilkin eded sayilir, yeni deposit olaraq bu qeder var, novbetileri uzerine gel.
+// accumulator -> deyerleri ozunde cemleyib, sonra return olunacaq parametrdir.
+// eger {initial} varsa {accumulator} 1-ci ona beraber olur, sonra array-in item-lerini uzerine gelir,
+// yoxdursa, default array-in 1ci item-ine beraber olur ve cemlenme 2-ciden baslayir.
+// ({accumulator} static variabledir)
+
+let numsForReduce = [5, 10, 15, 20, 25, 30];
+console.log("let numsForReduce = ");
+console.log(numsForReduce);
+console.log(`let reduceResult = numsForReduce.reduce((sum, current, index) => {
+  console.log(\`\${index + 1}. sum: \${sum}, current: \${current};\`);
+  console.log(" ");
+  return sum + current;
+}, 5);`);
+console.log(" ");
+let reduceResult = numsForReduce.reduce((sum, current, index) => {
+  console.log(`${index + 1}. sum: ${sum}, current: ${current};`);
+  console.log(" ");
+  return sum + current;
+}, 5);
+console.log("reduceResult: ");
+console.log(reduceResult);
+console.log(" ");
+console.log(`let listStr = names.reduce((ul, li, index) => {
+  return ul + \`<li>\${index + 1}. \${li}</li>\n\`;
+}, "");`);
+console.log(" ");
+let reduseListStr = names.reduce((ul, li, index) => {
+  return ul + `<li>${index + 1}. ${li}</li>\n`;
+}, "");
+console.log("reduseListStr: ");
+console.log(reduseListStr);
+let namesReduse = document.getElementById("namesReduse");
+namesReduse.innerHTML = reduseListStr;
+
+console.log(" ");
+console.log(`let reduseRightListStr = names.reduceRight((ul, li, index) => {
+  return ul + \`<li>\${index + 1}. \${li}</li>\n\`;
+}, "");`);
+console.log(" ");
+
+let reduseRightListStr = names.reduceRight((ul, li, index) => {
+  return ul + `<li>${index + 1}. ${li}</li>\n`;
+}, "");
+console.log("reduseRightListStr: ");
+console.log(reduseRightListStr);
+let namesReduseRight = document.getElementById("namesReduseRight");
+namesReduseRight.innerHTML = reduseRightListStr;
 
 console.log(" ");
