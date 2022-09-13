54ac574ef9e56649cb83edf17d56a46035a486c9
gabilgg07 <gabilgg07@gmail.com>
2022-09-13 11:47:49 +0400

    04. Countdown - timer

--- 03_JavaScript/6_JSON,date,cookie,session,localStorage/assets/js/04_Timer.js
+++ 03_JavaScript/6_JSON,date,cookie,session,localStorage/assets/js/04_Timer.js
@@ -1,1 +1,63 @@
 "use strict";
+
+const timers = document.querySelectorAll(".timer-info");
+
+function setCountdown(deadline = new Date(2022, 8, 15, 18)) {
+  const currentDate = new Date();
+
+  const diff = deadline - currentDate;
+
+  if (diff < 1000) {
+    // h4Day.innerText = 0;
+    // h4Hour.innerText = 0;
+    // h4Min.innerText = 0;
+    // h4Sec.innerText = 0;
+
+    // return; => bu da isleyir
+    clearInterval(beginCountdown);
+  }
+
+  const msOnDay = 1000 * 60 * 60 * 24;
+  const msOnHour = 1000 * 60 * 60;
+  const msOnMinute = 1000 * 60;
+  const msOnSec = 1000;
+
+  const days = Math.floor(diff / msOnDay);
+  const hours = Math.floor((diff % msOnDay) / msOnHour);
+  const minutes = Math.floor((diff % msOnHour) / msOnMinute);
+  const seconds = Math.floor((diff % msOnMinute) / msOnSec);
+
+  const values = [days, hours, minutes, seconds];
+
+  timers.forEach((h4, index) => {
+    h4.innerHTML = values[index];
+  });
+}
+setCountdown();
+let beginCountdown = setInterval(() => {
+  setCountdown();
+}, 1000);
+
+const counttime = document.querySelector("#counttime");
+
+counttime.addEventListener("change", function (e) {
+  const newSetDate = Date.parse(this.value);
+  const now = new Date();
+  const diffTime = newSetDate - now;
+
+  console.log();
+  if (diffTime < 0) {
+    alert("Please, take future time.");
+  } else {
+    clearInterval(beginCountdown);
+
+    let randomClr = randomColor() + "80";
+    timers.forEach((h4) => (h4.style.backgroundColor = randomClr));
+
+    setCountdown(newSetDate);
+
+    beginCountdown = setInterval(() => {
+      setCountdown(newSetDate);
+    }, 1000);
+  }
+});
--- 03_JavaScript/6_JSON,date,cookie,session,localStorage/assets/js/common.js
+++ 03_JavaScript/6_JSON,date,cookie,session,localStorage/assets/js/common.js
@@ -44,3 +44,31 @@ function createNewElements(elemName, selector = "") {
 
   return elems;
 }
+
+function randomColor() {
+  const hex = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
